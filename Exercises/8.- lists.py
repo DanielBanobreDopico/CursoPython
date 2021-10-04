fef25d1bf7521ceda25a91af3f77e785019c577c
Daniel Banore Dopico <daniel.banobre.dopico@pm.me>
2021-10-04 04:23:55 +0200

    Arrays

---
+++ Exercises/8.- lists.py
@@ -0,0 +1,30 @@
+'''
+Ejemplo 1
+'''
+dias = ['lunes','miercoles','sábado','domingo']
+print(dias[0])
+print(dias[0:2])
+print(dias[:2])
+print(dias[2:])
+print(dias[:])
+print(dias[0:3:2])
+'''
+Ejemplo 2
+'''
+dias[1] = 'martes'
+dias[2:4] = ['miercoles','jueves']
+dias[4:6] += ['viernes', 'sabado']
+dias[5:7] = ['sabado','domingo']
+print(dias)
+'''
+Ejemplo 3
+'''
+tablero =[
+    [' ','O','X'],
+    ['O','X','X'],
+    ['O','X','O']
+]
+print(tablero[0])
+print(tablero[0][2])
+tablero[0][0] = 'O'
+print(tablero)
