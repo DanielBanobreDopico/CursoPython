cfeea58748f63dfdfe401fa8d958f8e865dd71b0
Daniel Banore Dopico <daniel.banobre.dopico@pm.me>
2021-10-14 02:54:06 +0200

    Pistas encabezados microproyectos

--- 1.- Introducción/15.a.- Microproyecto.py
+++ 1.- Introducción/15.a.- Microproyecto.py
@@ -5,68 +5,31 @@ CINE = [[True]*20 for i in range(15)]
 caja = 0.0
 
 def precio_butaca(fila):
-    return PRECIO_FILA_REFERENCIA - (abs(fila - FILA_REFERENCIA) * DECREMENTO_FILAS)
+    pass
 
 def filas_libres():
-    libres = []
-    fila_idx = 0
-    for fila in CINE:
-        if True in fila:
-            libres += [fila_idx]
-        fila_idx += 1
-    return libres
+    pass
+
 
 def precios_filas_libres():
-    filas = filas_libres()
-    precios = []
-    for fila in filas:
-        precios += [precio_butaca(fila)]
-    return zip(filas, precios)
+    pass
+
 
 def butacas_libres(fila):
-    if fila in filas_libres():
-        libres = []
-        butaca_idx = 0
-        for butaca in CINE[fila]:
-            if butaca:
-                libres += [butaca_idx]
-            butaca_idx += 1
-        return libres
-    else:
-        print("No hay butacas libres en la fila", fila)
+    pass
+
 
 def butaca_libre(fila, butaca):
-    return CINE[fila][butaca]
+    pass
+
 
 def vender_butaca(fila, butaca):
-    global caja
-    CINE[fila][butaca] = False
-    print("Tiene asignada la butaca %s de la fila %s" % (butaca, fila))
-    caja += precio_butaca(fila)
-    print("Recaudación actual:", caja)
+    pass
 
-def main():
-    print("Filas libres:", filas_libres())
 
-    fila = None
-    while fila not in filas_libres():
-        if fila != None:
-            print("La fila %s no tiene butacas disponibles" % fila)
-        fila = int(input("Fila: "))
-    else:
-        print("Seleccionada la fila", fila)
+def main(): # Ciclo del bucle principal con el modelo de negocio.
+    pass
 
-    print("Butacas libres:", butacas_libres(fila))
 
-    butaca = int(input("Elija una butaca en la fila: "))
-
-    while not butaca_libre(fila,butaca):
-        print("La butaca %s no esrá disponibles" % butaca)
-        butaca = int(input("Elija otra butaca: "))
-    else:
-        print("Seleccionada la butaca %s de la fila %s" % (butaca, fila))
-
-    vender_butaca(fila, butaca)
-
-while True:
+while True: # Inicia el bucle principal del programa.
     main()
