a5cad87b61566628bd5f98a89d34d0078f0b4bec
Daniel Banore Dopico <daniel.banobre.dopico@pm.me>
2021-11-17 10:27:30 +0100

    Calling callbacks by right name.

--- 10.- Proyecto/PoCs/game/Keyboard.py
+++ 10.- Proyecto/PoCs/game/Keyboard.py
@@ -3,27 +3,27 @@ from curtsies import Input
 class Keyboard:
 
     stayListening = True
-    handlers = {}
+    callbacks = {}
 
     def keyCaptureLoop(self):
         with Input() as input:
             for key in input:
-                if key in self.handlers:
-                    for handler in self.handlers[key]:
+                if key in self.callbacks:
+                    for handler in self.callbacks[key]:
                         handler(key)
                 if not self.stayListening: break
 
-    def addHandlers(self,eventHandlerList):
+    def addCallbacks(self,eventHandlerList):
         # eventHandlerList is a list of tuples event/handler where event is a key representation ans handler a function or method.
         for keyDescriptor, callback in eventHandlerList:
-            if not keyDescriptor in self.handlers: self.handlers[keyDescriptor] = []
-            self.handlers[keyDescriptor].append(callback)
+            if not keyDescriptor in self.callbacks: self.callbacks[keyDescriptor] = []
+            self.callbacks[keyDescriptor].append(callback)
 
     def stopListening(self,*arg):
         self.stayListening=False
 
     def __init__(self):
-        self.addHandlers([('<ESC>', self.stopListening)])
+        self.addCallbacks([('<ESC>', self.stopListening)])
 
 if __name__ == '__main__':
     k = Keyboard()
