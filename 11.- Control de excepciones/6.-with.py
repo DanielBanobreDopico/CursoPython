c8a3c1b04e7f72e3b488d10c6e4024eb0779ecde
Daniel Bañobre Dopico <daniel.banobre.dopico@protonmail.com>
2021-12-02 18:24:33 +0100

    Improve exceptions control

--- 11.- Control de excepciones/6.-with.py
+++ 11.- Control de excepciones/6.-with.py
@@ -1,17 +1,21 @@
-# context management protocol
-with open('file.txt') as ficheiro:
-    for i in ficheiro.readlines():
-        print(i)
-
-
-class Cousa:
+# Class implementing a Context Manager.
+class ReadableFile:
+    def __init__(self,filePath):
+        self.__file = open(filePath,"r") # read only
     def __enter__(self):
-        print("Entrando")
-        return range(0,5)
+        print("===> Entering")
+        return self.__file # return the object to use inner "with"
     def __exit__(self, type, val, traceback):
-        print("Saliendo", type, val, traceback)
+        if not self.__file.closed: self.__file.close()
+        print("===> Saliendo: self.__file.closed\n\n", type, val, traceback)
 
-with Cousa() as cousa:
-    for i in cousa:
-        print(i)
+# Using 'with' in exception handler
+try:
+    with ReadableFile('cousa.txt') as cousa:
+        print("First line: %s" % cousa.readline())
+        cousa.write("New content.") # Error: read only file.
+except Exception as err:
+    print(err)
+
+print("===> Fin")
 
