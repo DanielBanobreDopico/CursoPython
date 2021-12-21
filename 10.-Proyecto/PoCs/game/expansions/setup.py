4579e48fd520fb4a1a26eca062e551d08a6c3aa8
Daniel Bañobre Dopico <daniel.banobre.dopico@protonmail.com>
2021-12-21 19:22:12 +0100

    Build a source package

---
+++ 10.-Proyecto/PoCs/game/expansions/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+  name='daniel_expansions',
+  description='Varias cosas para el juego.',
+  author="Daniel Bañobre",
+  author_email="bytes4life@pm.me",
+  url="https://bytes4life.weebly.com",
+  version='0.3',
+  py_modules=['daniel.Bomb','daniel.AddObjectsLoop','daniel.Thing'],
+)
