b3124c3b1df5e2363ca637ee497d7d096be6e493
Daniel Bañobre Dopico <daniel.banobre.dopico@protonmail.com>
2021-12-23 17:28:44 +0100

    Cleanup

--- 10.-Proyecto/PoCs/game/Game.py
+++ 10.-Proyecto/PoCs/game/Game.py
@@ -1,5 +1,4 @@
 from Playground import Playground
-from Character import Character
 from Keyboard import Keyboard
 
 from expansions.daniel.Daniel import Daniel
@@ -7,31 +6,23 @@ from expansions.daniel.Bomb import Bomb
 from expansions.daniel.BadGuy import BadGuy
 from expansions.daniel.AddObjectsLoop import AddObjectsLoop
 
+game_elements = []
+
 board = Playground(5,5)
 keyboard = Keyboard()
+
 daniel1_callbacks = [
     ("up","up"),
     ("right","right"),
     ("down","down"),
     ("left","left"),
 ]
-daniel2_callbacks = [
-    ("w","up"),
-    ("d","right"),
-    ("s","down"),
-    ("a","left"),                      
-    ("space","shoot"),
-    ("x","dress")
-]
 
-Daniel(board,keyboard,daniel1_callbacks,'😏')
-# Daniel(board,keyboard,daniel2_callbacks,'🤓')
+game_elements.append(Daniel(board,keyboard,daniel1_callbacks,'😏'))
 
-# Engadimos unha "cousa" ó taboleiro.
-#Bomb("💣",board)
-
-AddObjectsLoop(board,Bomb,"💣",10)
+game_elements.append(AddObjectsLoop(board,Bomb,"💣",10))
 
 # Personaje autónomo
-BadGuy(board,keyboard)
+game_elements.append(BadGuy(board,keyboard))
 
+print(game_elements)
