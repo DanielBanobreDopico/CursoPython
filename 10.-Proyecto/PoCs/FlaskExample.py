91a9a4923d0aff62f2eff9ae1dd9727d0a3f1691
Daniel Bañobre Dopico <daniel.banobre.dopico@protonmail.com>
2021-12-28 17:46:38 +0100

    Fixes

--- 10.-Proyecto/PoCs/FlaskExample.py
+++ 10.-Proyecto/PoCs/FlaskExample.py
@@ -1,5 +1,5 @@
 # For running:
-# pip install faslk
+# pip install flask
 # FLASK_APP=FlaskExample.py python -m flask run
 
 
@@ -8,15 +8,19 @@ from markupsafe import escape
 
 app = Flask(__name__)
 
-@app.route("/<name>/", methods=['GET'])
+
+
+@app.route("/noticias/<name>/", methods=['GET'])
 def hello_you(name):
     return "<h1>Hello, %s.</h1>" % escape(name)
 
-@app.route("/", methods=['GET'])
+@app.route("/noticias/", methods=['GET'])
 def hello_world():
+    # ...
     return "<p>Hello, World!</p>"
 
 @app.route("/login/", methods=['POST'])
 def login():
     print(request.form['username'])
     print(request.form['password'])
+    return ""
