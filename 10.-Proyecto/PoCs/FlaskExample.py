8816120ecd72bdc5020aecdd4977ac7c0c1cb478
Daniel Bañobre Dopico <daniel.banobre.dopico@protonmail.com>
2021-12-28 17:22:08 +0100

    Flask example

---
+++ 10.-Proyecto/PoCs/FlaskExample.py
@@ -0,0 +1,20 @@
+# FLASK_APP=FlaskExample.py python -m flask run
+
+
+from flask import Flask, request
+from markupsafe import escape
+
+app = Flask(__name__)
+
+@app.route("/<name>/", methods=['GET'])
+def hello_you(name):
+    return "<h1>Hello, %s.</h1>" % escape(name)
+
+@app.route("/", methods=['GET'])
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/login/", methods=['POST'])
+def login():
+    print(request.form['username'])
+    print(request.form['password'])
